17c8c038357f2b6f6fb5a4adaeb2bce1ff52c47c
Codewithtechfrenz <techfrenz2025@gmail.com>
2026-02-18 19:13:03 +0530

    devtool disabled

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import disableDevtool from 'disable-devtool';
+disableDevtool({
+  url: 'https://www.time-pass.in/' // redirect if devtools opened
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
